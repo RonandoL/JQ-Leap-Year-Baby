85e2410b989b9cd2904907a9556b3d20093d8b31
Ronando Long <ronando.long@gmail.com>
2015-12-16 14:39:06 -0800

    Refactored

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,9 @@ var leapYear = function(year) {
 }
 
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
   }
-}
+};
